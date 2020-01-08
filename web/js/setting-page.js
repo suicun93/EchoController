4492e89558a86fb1e5dcd77b1a07eae8d601f1da
bach287 <bach287@gmail.com>
2020-01-08 16:25:55 +0900

    add set EPC form

--- web/js/setting-page.js
+++ web/js/setting-page.js
@@ -1,24 +1,28 @@
 function setName(device) {
     var name = $("#" + device + "-name").val();
-    $.ajax({
-        type: "POST",
-        url: "SetName",
-        data: device + "," + name,
-        contentType: "application/x-www-form-urlencoded;charset=utf-8",
-        dataType: "text",
-        success: function (response) {
-            $(".msg-wrapper").append(responseMsg(SUCCESS_STATUS, "Set name success!"));
-            closeMsg();
-            if (response == SUCCESS_STATUS) {
-                // Set new name that has been changed
-                $("#device-name").html(name + "設定")  ;
-                $("#" + device + "-menu-name").html(name);
+    if(name == null){
+        $.ajax({
+            type: "POST",
+            url: "http://192.168.52.2:8080/Collector/SetName",
+            data: device + "," + name,
+            contentType: "application/x-www-form-urlencoded;charset=utf-8",
+            dataType: "text",
+            success: function (response) {
+                $(".msg-wrapper").append(responseMsg(SUCCESS_STATUS, "Set name success!"));
+                closeMsg();
+                if (response == SUCCESS_STATUS) {
+                    // Set new name that has been changed
+                    $("#device-name").html(name + "設定")  ;
+                    $("#" + device + "-menu-name").html(name);
+                }
+            },
+            error: function (response) {
+                $(".msg-wrapper").append(responseMsg(FAIL_STATUS, "Set name failed!"));
+                closeMsg();
             }
-        },
-        error: function (response) {
-            $(".msg-wrapper").append(responseMsg(FAIL_STATUS, "Set name success!"));
-            closeMsg();
-        }
-    });
-
+        });
+    }else{
+        $(".msg-wrapper").append(responseMsg(FAIL_STATUS, "Set name failed!"));
+        closeMsg();
+    }
 }
